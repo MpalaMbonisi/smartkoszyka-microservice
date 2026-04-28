4848d613f8b2fa564b80b7391ffb8906a1e5f015
Mbonisi Mpala <mpalambonisi63@gmail.com>
2026-04-28 07:11:02 +0200

    chore(shopping-list-service): add security configuration and service properties
    - add stateless security configuration to permit all requests
    - set docker-java api version to 1.44 in properties
    - define product-service url for inter-service communication

---
+++ services/shopping-list-service/src/main/java/com/github/mpalambonisi/shoppinglist/ShoppingListApplication.java
@@ -0,0 +1,14 @@
+package com.github.mpalambonisi.shoppinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(
+    basePackages = {"com.github.mpalambonisi.shoppinglist", "com.github.mpalambonisi.common"})
+public class ShoppingListApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ShoppingListApplication.class, args);
+  }
+}
